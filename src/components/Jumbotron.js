51189f864a9d35ce9881777314768c4149fca9df
Valts Vilcans <valts03@inbox.lv>
2020-10-07 16:39:04 +0200

    Jumbotron

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Container,
+  Inner,
+  Pane,
+  Title,
+  Subtitle,
+  Image,
+  Item,
+} from "./Jumbotron.styles";
+
+export const Jumbotron = ({ children, direction = "row", ...otherProps }) => {
+  return (
+    <Item direction={direction}>
+      <Inner>{children}</Inner>
+    </Item>
+  );
+};
+
+Jumbotron.Container = function JumbotronContainer({ children, ...otherProps }) {
+  return <Container {...otherProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...otherProps }) {
+  return <Pane {...otherProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...otherProps }) {
+  return <Title {...otherProps}>{children}</Title>;
+};
+
+Jumbotron.Subtitle = function JumbotronSubtitle({ children, ...otherProps }) {
+  return <Subtitle {...otherProps}>{children}</Subtitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...otherProps }) {
+  return <Image {...otherProps} />;
+};
